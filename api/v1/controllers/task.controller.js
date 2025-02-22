c589e38ef93e90db834ab242cd5f49b7b235c062
nbvhung <nguyenbaviethung300805@gmail.com>
2025-02-23 02:08:52 +0700

    Sắp xếp theo tiêu chí

---
+++ api/v1/controllers/task.controller.js
@@ -0,0 +1,39 @@
+const Task = require("../models/task.model");
+
+// [GET] /api/v1/tasks
+module.exports.index = async (req, res) => {
+    const find = {
+        deleted: false
+    };
+
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+
+    // Sort
+    const sort = {};
+
+    if(req.query.sortKey && req.query.sortValue){
+        sort[req.query.sortKey] = req.query.sortValue;
+    }
+
+    const tasks = await Task.find(find).sort(sort);
+
+    res.json(tasks);
+}
+
+// [GET] /api/v1/tasks/detail/:id
+module.exports.detail = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const task = await Task.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        res.json(task);
+    } catch (error) {
+        console.log("Not Found!");
+    }
+}
