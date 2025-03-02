6a1f1ec6dbbae5219d0e60d98d8789702b7c496d
nbvhung <nguyenbaviethung300805@gmail.com>
2025-03-03 02:07:15 +0700

    Danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("OK123");
+import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv"; 
+import * as database from "./config/database"; // * : lay het cac ham trong database
+import Task from "./models/task.model";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+    const tasks = await Task.find({
+        deleted: false
+    });
+
+    res.json(tasks);
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
