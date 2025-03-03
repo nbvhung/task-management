525089cfa49a4d7e2374d208465c1464c128a6c6
nbvhung <nguyenbaviethung300805@gmail.com>
2025-03-03 12:23:57 +0700

    Lọc trạng thái + sort

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,9 +3,32 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
+    
+    // Find
+    interface Find {
+        deleted: boolean,
+        status?: string
+    };
+
+    const find: Find = {
+        deleted: false,
+    }
+
+    if(req.query.status){
+        find.status = req.query.status.toString();
+    }
+    // End Find
+
+
+    // Sort
+    const sort = {};
+    
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey = req.query.sortKey.toLocaleString(); // ép kiểu sang string
+        sort[sortKey] = req.query.sortValue;
+    }
+    // End Sort
+    const tasks = await Task.find(find).sort(sort);
 
     res.json(tasks);
 };
