0d677324562517ea71d930eb45e80aa85a1948a8
nbvhung <nguyenbaviethung300805@gmail.com>
2025-03-04 07:44:33 +0700

    Thêm công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -136,3 +136,23 @@ export const changeMulti = async (req: Request, res: Response) => {
         })
     }
 }
+
+
+export const create = async (req: Request, res: Response) => {
+    try {
+        const task = new Task(req.body);
+
+        const data = await task.save();
+
+        res.json({
+            code: 200,
+            message: "Tạo thành công!",
+            data: data
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi!"
+        });
+    }
+}
